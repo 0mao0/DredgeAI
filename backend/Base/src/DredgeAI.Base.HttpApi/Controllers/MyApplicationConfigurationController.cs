91fad1d7dc2db08f803276011a6c2667229f2462
陈云飞 <chenyunfei7950@outlook.com>
2026-09-09 15:51:35 +0800

    feat(api): 替换应用配置控制器，增强可扩展性
    - 新增 `MyApplicationConfigurationController` 替代默认的 `AbpApplicationConfigurationController`。
    - 调整路由为 `/api/base/application-configuration`，规范化模块命名。
    - 修改 Rider 配置，新增文件排除规则，优化开发体验。

---
+++ backend/Base/src/DredgeAI.Base.HttpApi/Controllers/MyApplicationConfigurationController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Volo.Abp.AspNetCore.Mvc.AntiForgery;
+using Volo.Abp.AspNetCore.Mvc.ApplicationConfigurations;
+using Volo.Abp.DependencyInjection;
+
+namespace DredgeAI.Controllers;
+
+[Dependency(ReplaceServices = true)]
+[ExposeServices(typeof(AbpApplicationConfigurationController), IncludeSelf = true)]
+[Route($"api/{DredgeAIBaseRemoteServiceConsts.ModuleName}/application-configuration")]
+[Tags("应用配置")]
+public class MyApplicationConfigurationController : AbpApplicationConfigurationController
+{
+    public MyApplicationConfigurationController(
+        IAbpApplicationConfigurationAppService applicationConfigurationAppService,
+        IAbpAntiForgeryManager antiForgeryManager) : base(applicationConfigurationAppService, antiForgeryManager)
+    {
+    }
+}
